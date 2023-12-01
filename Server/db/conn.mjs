dfea127f076af55dea2e1d2ac4dd5b0b0d665ce2
Phromphithak <intermc2546@gmail.com>
2023-12-01 19:15:40 +0700

    Update

--- Server/db/conn.mjs
+++ Server/db/conn.mjs
@@ -1,35 +1,17 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI || "";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connectionString = process.env.ATLAS_URI || "";
 
-async function connectToDatabase() {
-  try {
-    console.log("Connecting to MongoDB Atlas...");
-    const conn = await client.connect();
-    console.log("Connected to MongoDB Atlas!");
-    return conn;
-  } catch (e) {
-    console.error("Error connecting to MongoDB Atlas:", e);
-    throw e;
-  }
-}
+const client = new MongoClient(connectionString);
 
 let conn;
 try {
-  conn = await connectToDatabase();
-} catch (e) {
+  console.log("Connecting to MongoDB Atlas...");
+  conn = await client.connect();
+} catch(e) {
   console.error(e);
-  // ในกรณีที่มีข้อผิดพลาด, คุณสามารถเลือกจะทำอะไรต่อไปได้ตามที่คุณต้องการ
-  // เช่น การล็อกข้อผิดพลาด, การทำงานแบบ graceful shutdown, หรือการ throw ต่อ
-  process.exit(1); // จบการทำงานทั้งโปรแกรม
 }
 
 let db = conn.db("hrmdata");
-
-// ทำงานที่ต้องการกับฐานข้อมูล
-
-// ปิดการเชื่อมต่อหลังจากใช้เสร็จสิ้น
-await client.close();
 
 export default db;
