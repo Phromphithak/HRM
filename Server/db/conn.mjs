023509bf8772cb6b546b8a186428291ccdac5ef6
Phromphithak <intermc2546@gmail.com>
2023-12-01 18:12:36 +0700

    fix

--- Server/db/conn.mjs
+++ Server/db/conn.mjs
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.ATLAS_URI || "";
+const uri = process.env.MONGODB_URI || "";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
