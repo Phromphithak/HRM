3c591e7659d45a673db9fe563b9048e0b9121352
XIL-LIN <116824046+Phromphithak@users.noreply.github.com>
2023-12-08 09:36:44 +0700

    fix

--- Server/server.mjs
+++ Server/server.mjs
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: "https://hrm-xil-lin.vercel.app/", // frontend URI (ReactJS)
 }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/record", records);
