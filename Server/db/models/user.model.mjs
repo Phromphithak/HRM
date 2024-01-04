18f01dcb96dff9c7eb26ecf0ae88eac756008fea
Phromphithak <intermc2546@gmail.com>
2024-01-05 00:21:11 +0700

    edit user.model

--- Server/db/models/user.model.mjs
+++ Server/db/models/user.model.mjs
@@ -1,15 +1,39 @@
-// models/user.model.mjs
-import mongoose from "mongoose";
+// models/User.js
 
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  email: { type: String, unique: true },
-  password: String,
-  photoURL: String,
-  role : String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'employee'],
+    default: 'employee',
+  },
+  profileImage: {
+    type: String, // เก็บ URL ของรูปภาพหรือ path ไปยังรูปภาพบนเซิร์ฟเวอร์
+    default: `/assets/images/avatars/avatar_${index + 1}.jpg`, // รูปภาพเริ่มต้น (ถ้ามี)
+  },
+  // อื่น ๆ ที่คุณต้องการเก็บสำหรับผู้ใช้
+  // ...
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
-export default User;
+module.exports = User;
--- src/sections/payroll/view/payroll-view.jsx
+++ src/sections/payroll/view/payroll-view.jsx
@@ -169,13 +169,12 @@ export default function PayrollPage() {
                 onSelectAllClick={handleSelectAllClick}
                 headLabel={[
                   { id: 'name', label: 'Name' },
-                  { id: 'employmentType', label: 'Type' },
+                  { id: 'employmentType', label: 'ประเภทการจ้างงาน' },
                   { id: 'position', label: 'ตำแหน่ง' },
                   { id: 'salary', label: 'เงินเดือน' },
                   { id: 'tax', label: 'หักเงิน'},
                   { id: 'bonus', label: 'โบนัส'},
-                  { id: 'isVerified', label: 'Verified', align: 'center' },
-
+                  { id: 'action', label: 'action', align: 'center' },
                 ]}
               />
               <TableBody>
